04190db63b19dfe550148986f22ce85ac2c53ea5
lordraiden <nono@dell.com>
2023-09-24 16:08:36 +0300

    home layout adjusted successfully

--- lib/home/home_layout.dart
+++ lib/home/home_layout.dart
@@ -1,3 +1,4 @@
+import 'package:clothes_store/component/component.dart';
 import 'package:clothes_store/pages/cart_screen.dart';
 import 'package:clothes_store/pages/home_screen.dart';
 import 'package:clothes_store/pages/menu_screen.dart';
@@ -14,57 +15,95 @@ class _HomeLayoutState extends State<HomeLayout> {
   int currentIndex = 0;
 
   final List<Widget> screens = [
-    const HomeScreen(),
     const CartScreen(),
     const MenuScreen(),
   ];
 
   final PageStorageBucket bucket = PageStorageBucket();
-  Widget currentScreen = const HomeScreen();
+  Widget currentScreen = const CartScreen();
+
   @override
   Widget build(BuildContext context) {
+    var isDesktop = MediaQuery.sizeOf(context).width > 500;
     return Scaffold(
       body: PageStorage(
         bucket: bucket,
         child: currentScreen,
       ),
       floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () {},
+        backgroundColor: currentIndex == 1 ? mainColor : Colors.white,
+        child: Icon(
+          Icons.home,
+          color: currentIndex == 1 ? Colors.white : Colors.grey,
+        ),
+        onPressed: () {
+          setState(() {
+            currentScreen = HomeScreen();
+            currentIndex = 1;
+          });
+        },
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       bottomNavigationBar: BottomAppBar(
         shape: CircularNotchedRectangle(),
         notchMargin: 10,
-        child: Container(
-          height: 60,
-          child: Row(
-            mainAxisAlignment:
-                MainAxisAlignment.spaceBetween, //main.spaceBetween
-            children: <Widget>[
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.start, //cross.start
-                children: [
-                  MaterialButton(
-                    onPressed: () {
-                      setState(() {
-                        currentScreen = HomeScreen();
-                        currentIndex = 0;
-                      });
-                    },
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center, //main.center
-                      children: [
-                        Icon(
-                          Icons.home,
-                          color: currentIndex == 0 ? Colors.amber : Colors.grey,
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: isDesktop ? 120 : 60),
+          child: Container(
+            alignment: Alignment.center,
+            height: 60,
+            child: Row(
+              //main.spaceBetween
+              children: <Widget>[
+                MaterialButton(
+                  onPressed: () {
+                    setState(() {
+                      currentScreen = CartScreen();
+                      currentIndex = 0;
+                    });
+                  },
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center, //main.center
+                    children: [
+                      Icon(
+                        Icons.shopping_cart,
+                        color: currentIndex == 0 ? mainColor : Colors.grey,
+                      ),
+                      Text(
+                        "Cart",
+                        style: TextStyle(
+                          color: currentIndex == 0 ? mainColor : Colors.grey,
                         ),
-                      ],
-                    ),
-                  )
-                ],
-              )
-            ],
+                      ),
+                    ],
+                  ),
+                ),
+                Spacer(),
+                MaterialButton(
+                  onPressed: () {
+                    setState(() {
+                      currentScreen = MenuScreen();
+                      currentIndex = 2;
+                    });
+                  },
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center, //main.center
+                    children: [
+                      Icon(
+                        Icons.menu,
+                        color: currentIndex == 2 ? mainColor : Colors.grey,
+                      ),
+                      Text(
+                        "Menu",
+                        style: TextStyle(
+                          color: currentIndex == 2 ? mainColor : Colors.grey,
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
